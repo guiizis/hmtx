417ecc4c325562ecce0469e7d1f75dd7a865937d
guiziis <guigamoda@hotmail.com>
2026-01-04 16:19:31 -0300

    feat: hx-select

--- 01-starting-project/app.js
+++ 01-starting-project/app.js
@@ -30,7 +30,11 @@ app.get('/', (req, res) => {
 
         <main>
           <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <form hx-post="/note" hx-target="ul" hx-swap="outerHTML">
+          <form 
+            hx-post="/note" 
+            hx-target="ul" 
+            hx-swap="outerHTML"
+            hx-select="ul">
             <p>
               <label for="note">Your note</label>
               <input type="text" id="note" name="note">
@@ -51,12 +55,13 @@ app.get('/', (req, res) => {
 app.post('/note', (req, res) => {
   const enteredNote = req.body.note;
   HTMX_KNOWLEDGE.unshift(enteredNote);
+  res.redirect('/');
 
-  res.send(`
-      <ul>
-        ${HTMX_KNOWLEDGE.map((info) => `<li>${info}</li>`).join('')}
-      </ul>
-    `);
+  // res.send(`
+  //     <ul>
+  //       ${HTMX_KNOWLEDGE.map((info) => `<li>${info}</li>`).join('')}
+  //     </ul>
+  //   `);
 });
 
 app.listen(3000);
